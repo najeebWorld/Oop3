c5ddd5fff5c5e4ef0b246710c0340637951da2d3
Yehudit-Brickner <nech.diti@yahoo.com>
2021-12-27 13:22:59 +0200

    chnged test node to fit the way the node is

--- NodeTest.py
+++ NodeTest.py
@@ -6,13 +6,12 @@ from Node import Node
 class MyTestCase(unittest.TestCase):
 
     def test_something(self):
-        n1 = Node(1, 2, 3, 1)
-        n2 = Node(3, 5, 6, 2)
+        n1 = Node((1, 2, 3), 1)
+        n2 = Node((3, 5, 6), 2)
         self.assertEqual(1,n1.getId())
         self.assertEqual(3, n2.getX())
         self.assertEqual(2, n1.getY())
-        n1.setX(2)
-        self.assertEqual(2, n1.getX())
+
 
 
 
